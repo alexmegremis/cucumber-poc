4b637d5066261e8acad09f6f2bdf6c99fe912df5
alexmegremis <alex.public@alexmegremis.com>
2020-02-24 14:37:21 +0000

    move to asserts

--- src/main/java/com/alexmegremis/funfun/api/BitwiseTest.java
+++ src/main/java/com/alexmegremis/funfun/api/BitwiseTest.java
@@ -1,5 +1,8 @@
 package com.alexmegremis.funfun.api;
 
+import org.springframework.util.Assert;
+
+import java.util.Arrays;
 import java.util.EnumSet;
 
 public class BitwiseTest {
@@ -15,14 +18,23 @@ public class BitwiseTest {
         Integer sum = permissions.stream().map(PERMISSIONS :: ordinal).reduce(0, (a, b) -> a |= (1 << b));
         System.out.println(">>> " + sum);
 
-        System.out.println(hasPermission(sum, PERMISSIONS.READ));
-        System.out.println(hasPermission(sum, PERMISSIONS.CREATE_BRANCH));
-        System.out.println(hasPermission(sum, PERMISSIONS.COMMENT));
-        System.out.println(hasPermission(sum, PERMISSIONS.WRITE));
-        System.out.println(hasPermission(sum, PERMISSIONS.ADMIN));
+        Assert.isTrue(hasPermission(sum, getPermissionSum(PERMISSIONS.READ)));
+        Assert.isTrue(hasPermission(sum, getPermissionSum(PERMISSIONS.CREATE_BRANCH)));
+        Assert.isTrue(hasPermission(sum, getPermissionSum(PERMISSIONS.COMMENT)));
+        Assert.isTrue(hasPermission(sum, getPermissionSum(PERMISSIONS.CREATE_BRANCH, PERMISSIONS.COMMENT)));
+        Assert.isTrue(hasPermission(sum, getPermissionSum(PERMISSIONS.CREATE_REPO, PERMISSIONS.COMMENT)));
+        Assert.isTrue(!hasPermission(sum, getPermissionSum(PERMISSIONS.WRITE, PERMISSIONS.CREATE_REPO)));
+        Assert.isTrue(hasPermission(sum, getPermissionSum(PERMISSIONS.WRITE, PERMISSIONS.CREATE_BRANCH)));
+        Assert.isTrue(!hasPermission(sum, getPermissionSum(PERMISSIONS.WRITE)));
+        Assert.isTrue(!hasPermission(sum, getPermissionSum(PERMISSIONS.ADMIN)));
     }
 
-    static boolean hasPermission(final Integer sum, final PERMISSIONS permission) {
-        return (sum & 1 << permission.ordinal()) > 0;
+    static boolean hasPermission(final Integer sum, final Integer test) {
+        return (sum & test) > 0;
+    }
+
+    static Integer getPermissionSum(final PERMISSIONS... permissionTest) {
+        Integer result = Arrays.stream(permissionTest).map(PERMISSIONS :: ordinal).reduce(0, (a, b) -> a |= (1 << b));
+        return result;
     }
 }
